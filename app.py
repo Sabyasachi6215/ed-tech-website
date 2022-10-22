737c04c121f3c1a05e81f98cd2f784ead67663d0
Sabyasachi <54863241+Sabyasachi6215@users.noreply.github.com>
2022-10-22 15:32:31 +0000

    changes in layout

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+JOBS=[
+  {
+    'id':0,
+    'title':'Data Analyst',
+    'location':'Bangalore',
+    'salary':'Rs 6.5 lpa',
+  },
+  {
+    'id':1,
+    'title':'Data Scientist',
+    'location':'Mumbai',
+    'salary':'Rs 10.5 lpa',
+  },
+  {
+    'id':2,
+    'title':'DevOps Engineer',
+    'location':'Delhi',
+    'salary':'Rs 12.5 lpa',
+  },
+  {
+    'id':3,
+    'title':'Data Engineering',
+    'location':'Germany',
+    'salary':'Euro 5k',
+  }
 
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
 
 
 if __name__ == '__main__':
